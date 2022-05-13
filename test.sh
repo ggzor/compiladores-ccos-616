edfac5b735ed3f48c95164f635a1f18fa4bb5024
ggzor <30713864+ggzor@users.noreply.github.com>
2022-05-13 16:57:50 -0500

    Add support for golden tests

--- test.sh
+++ test.sh
@@ -12,17 +12,60 @@ run() {
     | java Semantic
 }
 
-for f in ./samples/final/test_fail/*; do
+print_file_error() {
+  printf '\e[1;31m[NO]\e[0m \e[2m%s\e[0m %s\n' "$2" "$1"
+}
+
+print_file_warning() {
+  printf '\e[1;33m[HM]\e[0m \e[2m%s\e[0m %s\n' "$2" "$1"
+}
+
+print_file_ok() {
+  printf '\e[1;32m[OK]\e[0m %s\n' "$1"
+}
+
+for f in ./samples/final/test_fail/*.txt; do
   set +e
   OUTPUT=$(run "$f" 2>&1)
   EXIT_CODE=$?
   set -e
 
   if (( $EXIT_CODE != 0 )); then
-    printf '\e[1;32m[OK]\e[0m %s\n' "$f"
+    print_file_ok "$f"
   else
-    printf '\e[1;31m[NO]\e[0m \e[2mShould fail\e[0m %s\n' "$f"
+    print_file_error "$f" "Should fail"
     printf '%s\n' "$OUTPUT"
+    exit 1
+  fi
+done
+
+
+for f in ./samples/final/test_ok/*.txt; do
+  TMP=$(mktemp)
+  EXPECTED="${f%.txt}.out"
+
+  set +e
+  run "$f" &> "$TMP"
+  EXIT_CODE=$?
+  set -e
+
+  if (( $EXIT_CODE == 0 )); then
+    if git ls-files --error-unmatch "$EXPECTED" &> /dev/null; then
+      if diff "$EXPECTED" "$TMP" &> /dev/null; then
+        print_file_ok "$f"
+      else
+        print_file_error "$f" "Should match previous file"
+        cat "$TMP"
+        exit 1
+      fi
+    else
+      print_file_warning "$f" "Output is not tracked by git"
+      cat "$TMP"
+      mv "$TMP" "$EXPECTED"
+    fi
+  else
+    print_file_error "$f" "Should compile correctly"
+    cat "$TMP"
     exit 1
   fi
 done
